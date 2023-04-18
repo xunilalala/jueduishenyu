3c15c81c02902a3907bec512ff92e8ebb0b70a13
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-18 18:51:29 +0800

    Update jueduishenyu.py

--- Spider/jueduishenyu.py
+++ Spider/jueduishenyu.py
@@ -5,64 +5,53 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import os
 import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chromedriver = "/usr/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
 
-# 创建Chrome浏览器对象
-driver = webdriver.Chrome()
-driver.maximize_window()
-# 打开网页
-
-
-# 用户名密码表
 users = [
-    {'username': 'cuishi', 'password': 'yi-KY.T:qr6iMwy'},
-    {'username': 'daishen', 'password': 'miMJhPcv5MA!u!w'},
-    {'username': 'quechao', 'password': 't5W2Sw5vrH:X6A-'},
-    {'username': 'songhao', 'password': 'TZd!FXrvqSja2b6'},
-    {'username': 'tianshu', 'password': 'R!234n-bs3.anQD'},
-    {'username': 'xuni', 'password': 'AdfG:CaufU6inec'},
-    {'username': 'yituodabian', 'password': '2Ncx-sm_8jefX7r'},
-    {'username': 'licheng', 'password': '-m-eeDki8x8CprT'}
+    {'username': 'xuni512', 'password': 'ml,152ml.636'},
+    {'username': 'xuni', 'password': 'ml,152ml.636'},
+    {'username': 'xuni1024', 'password': 'ml,152ml.636'},
+    {'username': 'xuni256', 'password': 'ml,152ml.636'}
 ]
 
 for user in users:
-    # 创建Chrome浏览器对象
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    # 打开网页
+    driver = webdriver.Chrome(options=chrome_options,executable_path=chromedriver)
 
-    # 打开网页
-    driver.get('https://www.sdhhz.cc/')
-    # 点击登录按钮
+    # 访问网页
+    driver.get("http://www.acg088.com/user/coin")
+
+    # 等待登录框出现
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "username"))
+    )
+
+    # 输入账户和密码并登录
+    username = driver.find_element_by_name("username")
+    username.send_keys(user['username'])
+    password = driver.find_element_by_name("password")
+    password.send_keys(user['password'])
+    driver.find_element_by_class_name("go-login").click()
+    print(f"{user['username']}完成登录")
+
     time.sleep(10)
-    login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'empty')))
-    login_button.click()
-    # 在登录页面中输入用户名和密码并点击登录按钮
-    username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
-    password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
-    username_input.send_keys(user['username'])
-    password_input.send_keys(user['password'])
-    print(f"Logging in as {user['username']}")
-    login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[text()="快速登录"]')))
-    login_button.click()
-    time.sleep(8)
-    # 点击今日签到
-    sign_in_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="bar-item bar-mission"]')))
-    sign_in_button.click()
-    # 点击领取签到奖励
-    time.sleep(3)
-    reward_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="bar-user-info-row bar-mission-action"]')))
-    reward_button.click()
-    # 关闭浏览器
+    # 等待关闭按钮出现
+
+    WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.CLASS_NAME, "swal2-close")))
+    driver.execute_script("document.querySelector('.swal2-close').click()")
+
+    # 等待签到按钮出现并点击
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "go-user-qiandao"))
+    )
+    driver.find_element_by_class_name("go-user-qiandao").click()
     driver.quit()
-    time.sleep(10)
-    print(user['username']+"完成签到")
+    print(f"{user['username']}签到完成")
 
-print("所有用户签到完成!")
+print("所有用户签到完成")
