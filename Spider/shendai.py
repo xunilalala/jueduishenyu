b01fda3f8a6b845e86d17f53167366023d64f93d
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-18 20:36:25 +0800

    Update shendai.py

--- Spider/shendai.py
+++ Spider/shendai.py
@@ -19,7 +19,16 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 # 用户名密码表
 users = [
     
-    {'username': 'chunlei', 'password': 'j54Z6F!RY6j8U:6'}
+    {'username': 'chunlei', 'password': 'j54Z6F!RY6j8U:6'},
+    {'username': 'cuishi', 'password': 'yi-KY.T:qr6iMwy'},
+    {'username': 'daishen', 'password': 'miMJhPcv5MA!u!w'},
+    {'username': 'quechao', 'password': 't5W2Sw5vrH:X6A-'},
+    {'username': 'songhao', 'password': 'TZd!FXrvqSja2b6'},
+    {'username': 'tianshu', 'password': 'R!234n-bs3.anQD'},
+    {'username': 'xuni', 'password': 'AdfG:CaufU6inec'},
+    {'username': 'yituodabian', 'password': '2Ncx-sm_8jefX7r'},
+    {'username': 'licheng', 'password': '-m-eeDki8x8CprT'}
+   
 ]
 for user in users:
     # 创建Chrome浏览器对象
