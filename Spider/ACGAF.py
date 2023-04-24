705eea6fdea015c9f47b1cdb2642892a3c38a8a5
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-24 08:00:25 +0800

    Update ACGAF.py

--- Spider/ACGAF.py
+++ Spider/ACGAF.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
