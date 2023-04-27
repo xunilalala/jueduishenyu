71ac36b0ce8bc8d5c9ef8c8087e516a936b10425
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-27 19:26:55 +0800

    Create main.py

---
+++ Spider/main.py
@@ -0,0 +1,17 @@
+import concurrent.futures
+import os
+
+def run_script(script_name):
+    os.system("python " + script_name)
+
+if __name__ == "__main__":
+    # 设置需要同时启动的py文件列表
+    script_list = ["ACGAF.py", "jueduishenyu.py", "shendai.py","yuzhai.py",""xingmeng.py","xinhuan.py",]
+
+    # 创建线程池
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(script_list)) as executor:
+        # 提交每个脚本的任务
+        futures = [executor.submit(run_script, script_name) for script_name in script_list]
+
+        # 等待所有任务完成
+        concurrent.futures.wait
