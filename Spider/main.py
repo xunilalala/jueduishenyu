0ac53f197d82b77793ed10c9bbb39498d026d5e6
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-04-28 10:50:42 +0800

    Update main.py

--- Spider/main.py
+++ Spider/main.py
@@ -6,7 +6,7 @@ def run_script(script_name):
 
 if __name__ == "__main__":
     # 设置需要同时启动的py文件列表
-    script_list = ["./Spider/ACGAF.py", "./Spider/jueduishenyu.py", "./Spider/shendai.py","./Spider/yuzhai.py","./Spider/xingmeng.py","./Spider/xinhuan.py",]
+    script_list = ["./Spider/ACGAF.py", "./Spider/jueduishenyu.py", "./Spider/shendai.py","./Spider/yuzhai.py","./Spider/xingmeng.py",]
 
     # 创建线程池
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(script_list)) as executor:
