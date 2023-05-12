6de25aec2861ef153a8553b115b1b2453dc270fe
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-05-12 08:41:49 +0800

    Update xinhuan.py

--- Spider/xinhuan.py
+++ Spider/xinhuan.py
@@ -1,68 +1,76 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import os
 import time
-from selenium.webdriver.common.action_chains import ActionChains
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/bin/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
+# 创建谷歌浏览器options对象，设置一些参数
+option = webdriver.ChromeOptions()
+option.add_experimental_option('excludeSwitches', ['enable-automation'])
+option.add_experimental_option('useAutomationExtension', False)
 
+# 创建浏览器对象
+browser = webdriver.Chrome(options=option)
 
-# 用户名密码表
-users = [
-   # {'username': 'xiajibaluanxiede@163.com', 'password': '-K42!FycASxT3mS'},
-   # {'username': '3120294679@qq.com', 'password': 'ml152636'}
-   # {'username': '18264410349@163.com', 'password': 'SWNNnq:mi5L6bHT'},
-   # {'username': 'wanzhanadmin@163.com', 'password': 'zwD2!D_PhnSWpYR'},
-   # {'username': 'xiangz91121i@163.com', 'password': '4_v8mee73RdRNrH'},
-    {'username': 'chengxinchengyi@1163.com', 'password': '5hRc.49smV!rq.y'}
+# 将webdriver属性置为undefined
+browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
+                        {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
 
-    
-]
+# 窗口最大化
+browser.maximize_window()
 
-for user in users:
-    # 创建Chrome浏览器对象
-    driver = webdriver.Chrome(options=chrome_options,executable_path=chromedriver)
-    driver.maximize_window()
-    # 打开网页
-    driver.get('https://xhcy.us/author/')
-    # 点击登录按钮
-    close_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.poi-dialog__header__close")))
-    close_button.click()
-    # 定位到需要点击的元素
-    login_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.inn-sign__login-btn__container a.inn-sign__login-btn")))
-    driver.execute_script("arguments[0].click();", login_button)
-    # 在登录页面中输入用户名和密码并点击登录按钮
-    email_input = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='email']")))
-    password_input = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='pwd']")))
-    email_input.send_keys(user['username'])
-    password_input.send_keys(user['password'])
-    print(f"Logging in as {user['username']}")
-    login_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
-    driver.execute_script("arguments[0].click();",login_button)
-    # close_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class, 'poi-dialog__footer__btn_default') and text()='关闭']")))
-    # driver.execute_script("arguments[0].click();", close_button)
-    # 点击签到
-    try:
-        sign_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.poi-tooltip.inn-nav__point-sign-daily__btn[title='签到']")))
-        x, y = sign_button.location_once_scrolled_into_view['x'], sign_button.location_once_scrolled_into_view['y']
-        actions = ActionChains(driver)
-        actions.move_by_offset(x, y).click().perform()
-        print(x)
-        print(y)
-    except:
-        print(user['username']+"签到失败")
+# 访问网址 https://xhcy.us/
+start_time = time.time()  # 记录开始运行时间
+browser.get("https://xhcy.us/")
 
-    # 关闭浏览器
-    driver.quit()
-    print(user['username']+"完成签到")
-    
-print("芯幻--完成")
+# 显示等待，等待关闭按钮出现
+close_button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.poi-dialog__footer__btn")))
+
+# 点击关闭按钮
+close_button.click()
+
+# 显示等待，等待登录按钮出现
+login_button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "inn-sign__login-btn")))
+
+# 点击登录按钮
+login_button.click()
+
+# 显示等待，等待邮箱输入框出现
+email_input = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.NAME, "email")))
+
+# 输入邮箱
+email_input.send_keys("xiangz91121i@163.com")
+
+# 显示等待，等待密码输入框出现
+password_input = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.NAME, "pwd")))
+
+# 输入密码
+password_input.send_keys("4_v8mee73RdRNrH")
+
+# 显示等待，等待登录按钮出现
+submit_button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.poi-dialog__footer__btn_success")))
+
+# 点击登录按钮
+submit_button.click()
+
+# 显示等待，等待关闭按钮出现
+close_button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.poi-dialog__footer__btn")))
+
+# 点击关闭按钮
+close_button.click()
+
+wait = WebDriverWait(browser, 10)
+button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#inn-nav__point-sign-daily a')))
+button.click()
+
+# 显示等待，等待关闭按钮出现
+close_button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.poi-dialog__footer__btn")))
+
+# 点击关闭按钮
+close_button.click()
+
+end_time = time.time()  # 记录结束运行时间
+print(f"代码执行时间为：{end_time - start_time}秒")
+
+# 关闭浏览器
+browser.quit()
