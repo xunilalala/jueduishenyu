4ae36a7caa2815d16c9856c877867c54ea2a8738
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-05-12 08:52:22 +0800

    Update xinhuan.py

--- Spider/xinhuan.py
+++ Spider/xinhuan.py
@@ -10,7 +10,7 @@ option.add_experimental_option('excludeSwitches', ['enable-automation'])
 option.add_experimental_option('useAutomationExtension', False)
 chromedriver = "/usr/bin/chromedriver"
 # 创建浏览器对象
-browse = webdriver.Chrome(options=option,executable_path=chromedriver)
+browser = webdriver.Chrome(options=option,executable_path=chromedriver)
 
 # 将webdriver属性置为undefined
 browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
