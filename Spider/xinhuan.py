c4dced2b7d685ca1043dbaecdc77e1c84c0b9592
旭霓 <64685627+xunilalala@users.noreply.github.com>
2023-06-18 20:29:04 +0800

    Update xinhuan.py

--- Spider/xinhuan.py
+++ Spider/xinhuan.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 # 创建谷歌浏览器options对象，设置一些参数
 option = webdriver.ChromeOptions()
@@ -11,17 +13,17 @@ option.add_experimental_option('excludeSwitches', ['enable-automation'])
 option.add_experimental_option('useAutomationExtension', False)
 option.add_argument('--no-sandbox')
 option.add_argument('--disable-dev-shm-usage')
-option.add_argument('--headless')
+#option.add_argument('--headless')
 chromedriver = "/usr/bin/chromedriver"
 
 # 多个用户登录信息
 users = [
-    {'email': '18264410349@163.com', 'password': 'SWNNnq:mi5L6bHT'},
-    {'email': '3120294679@qq.com', 'password': 'ml152636'},
-    {'email': 'chengxinchengyi@163.com', 'password': '5hRc.49smV!rq.y'},
-    {'email': 'wanzhanadmin@163.com', 'password': 'zwD2!D_PhnSWpYR'},
-    {'email': 'xiangz91121i@163.com', 'password': '4_v8mee73RdRNrH'},
-    {'email': 'xiajibaluanxiede@163.com', 'password': '-K42!FycASxT3mS'}
+    #{'email': '18264410349@163.com', 'password': 'SWNNnq:mi5L6bHT'},
+    {'email': '3120294679@qq.com', 'password': 'ml152636'}
+    #{'email': 'chengxinchengyi@163.com', 'password': '5hRc.49smV!rq.y'},
+    #{'email': 'wanzhanadmin@163.com', 'password': 'zwD2!D_PhnSWpYR'},
+    #{'email': 'xiangz91121i@163.com', 'password': '4_v8mee73RdRNrH'},
+    #{'email': 'xiajibaluanxiede@163.com', 'password': '-K42!FycASxT3mS'}
 ]
 
 for user in users:
@@ -87,21 +89,25 @@ for user in users:
         print(f"{user['email']}签到成功")
     except:
         print(f"{user['email']}签到失败")
+    time.sleep(10)
     try:
         # 定位要点击的元素
-        element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//h3[contains(text(), '兑换')]")))
-
-        # 等待元素出现后点击
+        element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.inn-account__lottery__item.poi-list__item h3.inn-account__lottery__item__title')))
         element.click()
 
-        button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "poi-btn_success")))
 
+        button = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "poi-btn_success")))
+        # 使用ActionChains类进行滚动到底部操作
+        # 模拟按下Page Down键来滚动页面
+
+        browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
         # 点击元素
+        element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'inn-user-code__container')))
+        time.sleep(5)
         button.click()
     except:
         print(f"{user['email']}芯币领取失败")
     end_time = time.time()  # 记录结束运行时间
     print(f"{user['email']}代码执行时间为：{end_time - start_time}秒")
-    
     # 关闭浏览器
     browser.quit()
